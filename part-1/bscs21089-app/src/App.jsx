0d156d921dccce0f4a8ab74a4a77f28d9a3abe2f
bsee23055 <bscs21089@itu.edu.pk>
2024-10-09 13:24:30 +0500

    assign-2

--- part-1/bscs21089-app/src/App.jsx
+++ part-1/bscs21089-app/src/App.jsx
@@ -1,13 +1,42 @@
+import React from 'react';
+import SalesCard from "./components/salescard.jsx";
+import Input from "./components/input.jsx";
+import DropdDown from './components/dropdown-menu.jsx';
+import PrimaryButton from './components/primary-button.jsx';
+import TextArea from './components/TextArea.jsx';
+import LeftSideBar from './components/left-sidebar-button.jsx';
+import ReactionBar from './components/ReactionBar.jsx';
+import PostHeader from './components/post-Header.jsx';
+import MenuItem from './components/MenuItem.jsx';
+import ReviewItem from './components/review-card.jsx';
+import VideoSection from './components/video-post.jsx';
+import Res_c_item from './components/res-card-item.jsx';
 
-import Res_c_item from "./components/res-card-item"
 
-function App() {
-  
-
+const Admin = () => {
   return (
-    <Res_c_item/>
+    <div >
+      <Res_c_item />
+      <DropdDown />
+      <Input />
+      <PrimaryButton />
+      <SalesCard />
+      <TextArea />
+      <ReactionBar />
+      <LeftSideBar />
+      <PostHeader />
+      <ReviewItem />
+      <MenuItem />
+      <VideoSection />
 
-  )
-}
 
-export default App
+
+    </div>
+  );
+};
+
+
+
+export default Admin;
+
+
