e7a23c8a49389fb6aa281eccdc594ec1269e4efd
bsee23055 <bscs21089@itu.edu.pk>
2024-10-09 13:15:30 +0500

    res menu comp create

---
+++ part-1/bscs21089-app/src/components/res-card-item.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+const Res_c_item = () => {
+    return (
+
+        <section id="menu" class="menu section">
+
+
+            <div class="container">
+
+
+                <div class="tab-content" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="tab-pane fade active show" id="menu-starters">
+
+
+
+                        <div class="row gy-5">
+
+                            <div class="col-lg-4 menu-item">
+                                <a href="assets/img/menu/menu-item-1.png" class="glightbox"><img src="/public/menu-item-1.png" class="menu-img img-fluid" alt=""/></a>
+                                <h4>Magnam Tiste</h4>
+                                <p class="ingredients">
+                                    Lorem, deren, trataro, filede, nerada
+                                </p>
+                                <p class="price">
+                                    $5.95
+                                </p>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>)
+}
+export default Res_c_item;
